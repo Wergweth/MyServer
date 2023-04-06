07b19c6af02a3a372fa61b99fb6b95444820f99b
Oleg <Wergweth@rambler.ru>
2023-04-06 15:24:47 +0300

    test

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	gin.Default()
+	sql.Open("mysql", "root:1q2w3e@(127.0.0.1:3306)/test1")
+	engine := gin.New()
+	engine.GET("", MyHandler)
+	engine.GET("/ebalo34", MyHandler2)
+	engine.Run("localhost:8080")
+}
+
+func MyHandler(c *gin.Context) {
+	c.String(200, "ebalo")
+}
+
+func MyHandler2(c *gin.Context) {
+	c.String(200, "Urrra")
 }
